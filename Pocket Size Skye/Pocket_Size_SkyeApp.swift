66a5f2d4380e548fc55aca7fb18884b0cd765ff9
Connor Dougan <connordougan.business@gmail.com>
2024-10-13 11:03:08 +0100

    Initial Commit

---
+++ Pocket Size Skye/Pocket_Size_SkyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_Size_SkyeApp.swift
+//  Pocket Size Skye
+//
+//  Created by Connor Dougan on 13/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Pocket_Size_SkyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
